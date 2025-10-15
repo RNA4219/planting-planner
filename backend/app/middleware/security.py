bea2165c1a04620d3c3e8747a84ad95495edd2da
R_N_A <33271130+RNA4219@users.noreply.github.com>
2025-10-16 02:53:40 +0900

    Fix backend lint issues

--- backend/app/middleware/security.py
+++ backend/app/middleware/security.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Mapping
+from collections.abc import Mapping
 
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
