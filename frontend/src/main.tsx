8e0620a67554715f3cf50ee42322ddcb91bcf499
R_N_A <33271130+RNA4219@users.noreply.github.com>
2025-10-17 07:54:47 +0900

    fix: remove unused idle callback helper

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,21 +6,6 @@ import App from './App'
 import './index.css'
 import { registerServiceWorker } from './lib/swClient'
 
-const scheduleAfterIdle = (callback: () => void) => {
-  const globalWithIdle = globalThis as typeof globalThis & {
-    requestIdleCallback?: (callback: IdleRequestCallback) => number
-  }
-
-  if (typeof globalWithIdle.requestIdleCallback === 'function') {
-    globalWithIdle.requestIdleCallback(() => {
-      callback()
-    })
-    return
-  }
-
-  setTimeout(callback, 0)
-}
-
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
